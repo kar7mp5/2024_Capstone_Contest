74119913358ee34702ac918911c965017e11ebae
kar7mp5 <tommy1005a@gmail.com>
2024-08-30 07:08:26 +0100

    merge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 from robot_control import Controller
-from robot_control import recognize_speech_from_mic, text_to_tts, gpt_api
+from robot_control import recognize_speech_from_mic, gpt_api
 
 
 import speech_recognition as sr
@@ -16,12 +16,11 @@ def main():
     """Main function to activate voice model on specific wake word."""
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
-
-    text_to_tts("잠시만 기다려 주세요. 주변 소음에 적응하는 중입니다.")
+    
+    print("Adjusting the environment sound")
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source, duration=5)
     
-    text_to_tts(f"모델 이름을 불러 명령할 수 있습니다. {MODEL_NAME}을 말해주세요.")
 
     controller = Controller(17, 22, 24, 23)
 
@@ -31,7 +30,6 @@ def main():
 
         # Get the text
         if response["transcription"] and MODEL_NAME in response["transcription"].lower():
-            text_to_tts("로봇이 활성화되었습니다. 명령을 주십시오.")
             response = recognize_speech_from_mic(recognizer, microphone)
             
             match gpt_api(response):
@@ -59,7 +57,6 @@ def main():
 
         # Exit the program
         if response["transcription"] and '종료' in response["transcription"].lower():
-            text_to_tts("프로그램을 종료합니다.")
             break
 
         if response["error"]:
--- robot_control/control.py
+++ robot_control/control.py
@@ -1,6 +1,5 @@
 # control.py
 import RPi.GPIO as gpio
-from pynput import keyboard
 import time
 
 
@@ -93,28 +92,3 @@ class Controller:
         gpio.output(self.br_pin, True)
         time.sleep(sec)
         gpio.cleanup()
-
-
-# key events
-def on_press(key):
-    try:
-        print('Alphanumeric key pressed: {0} '.format(key.char))
-    except AttributeError:
-        print('Special key pressed: {0}'.format(key))
-
-def on_release(key):
-    print('{0} released'.format(key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-
-
-
-if __name__=="__main__":
-
-    # Collect events until released
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-
-    controller = Controller(17, 22, 24, 23)
