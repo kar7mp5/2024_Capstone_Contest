faace4b0eac4006da759d7c0dec8a002a48b3bd7
kimminsum <tommy1005a@gmail.com>
2024-08-30 15:02:29 +0900

    Del audio detection

--- main.py
+++ main.py
@@ -1,69 +1,12 @@
 # main.py
 from robot_control import Controller
-from robot_control import recognize_speech_from_mic, text_to_tts, gpt_api
 
 
-import speech_recognition as sr
-
-
-
-
-MODEL_NAME: str = "시리"
-SEC: int = 2
 
 
 def main():
     """Main function to activate voice model on specific wake word."""
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-
-    text_to_tts("잠시만 기다려 주세요. 주변 소음에 적응하는 중입니다.")
-    with microphone as source:
-        recognizer.adjust_for_ambient_noise(source, duration=5)
-    
-    text_to_tts(f"모델 이름을 불러 명령할 수 있습니다. {MODEL_NAME}을 말해주세요.")
-
     controller = Controller(17, 22, 24, 23)
-
-    while True:
-        print("Listening...")
-        response = recognize_speech_from_mic(recognizer, microphone)
-
-        # Get the text
-        if response["transcription"] and MODEL_NAME in response["transcription"].lower():
-            text_to_tts("로봇이 활성화되었습니다. 명령을 주십시오.")
-            response = recognize_speech_from_mic(recognizer, microphone)
-            
-            match gpt_api(response):
-                case 1:             # front
-                    controller.forward()                                        
-                    break
-                
-                case 2:             # back
-                    controller.go_back()
-                    break
-                
-                case 3:             # left
-                    controller.left_turn()
-                    break
-                
-                case 4:             # right
-                    controller.right_turn()
-                    break
-                
-                case 5:             # etc
-                    print("Unacceptable output!!")
-                    break
-            
-            print(response)
-
-        # Exit the program
-        if response["transcription"] and '종료' in response["transcription"].lower():
-            text_to_tts("프로그램을 종료합니다.")
-            break
-
-        if response["error"]:
-            print("ERROR: {}".format(response["error"]))
 
 
 
