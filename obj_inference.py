4425e653d719f1a4be0675ec16e969a490c3f86f
kar7mp5 <tommy1005a@gmail.com>
2024-10-17 01:09:57 +0900

    Add the function of inferrening the image

--- obj_inference.py
+++ obj_inference.py
@@ -3,37 +3,51 @@ import roboflow
 from roboflow import Roboflow
 roboflow.login()
 
-from  dotenv import load_dotenv
-import os
-
 import random
 
 
-load_dotenv()
-API_KEY = os.environ.get("ROBOFLOW_KEY")
+def get_obj_inference(API_KEY, image_path):
+    """Get object inference result
+    
+    Args:
+        API_KEY (str): Roboflow API KEY
+        image_path (str): inference target image path
 
-# Initialize Roboflow with API key
-rf = Roboflow(api_key=API_KEY)
-project = rf.workspace().project("bottle-cap-nbpv9")
-dataset = project.version(1).download("yolov8")
+    Returns:
+        prediction (list[dict]): object inference results
+    """
+    # Initialize Roboflow with API key
+    rf = Roboflow(api_key=API_KEY)
+    project = rf.workspace().project("bottle-cap-nbpv9")
+    dataset = project.version(1).download("yolov8")
+
+    # Load the YOLOv8 model from the local directory
+    model = project.version(dataset.version).model
+
+    # Select a test image
+    random_test_image = random.choice(os.listdir(image_path))
+    print("Running inference on " + random_test_image)
+
+    # Execute prediction
+    pred = model.predict(os.path.join(image_path, random_test_image))
+    print(pred)
+
+    # Visualize prediction results
+    # Save the results as an image
+    output_image_path = 'output_image.png'
+    pred.save(output_image_path)
+
+    # Print the path to the saved image
+    print(f"Image saved to: {output_image_path}")
 
 
-# Load the YOLOv8 model from the local directory
-model = project.version(dataset.version).model
 
-# Select a test image
-test_set_loc = 'test_img/'  # Path where test images are located
-random_test_image = random.choice(os.listdir(test_set_loc))
-print("Running inference on " + random_test_image)
 
-# Execute prediction
-pred = model.predict(os.path.join(test_set_loc, random_test_image))
-print(pred)
+if __name__=="__main__":
+    from  dotenv import load_dotenv
+    import os
 
-# Visualize prediction results
-# Save the results as an image
-output_image_path = 'output_image.png'
-pred.save(output_image_path)
+    load_dotenv()
+    API_KEY = os.environ.get("ROBOFLOW_KEY")
 
-# Print the path to the saved image
-print(f"Image saved to: {output_image_path}")
+    get_obj_inference(API_KEY, "test_image")
